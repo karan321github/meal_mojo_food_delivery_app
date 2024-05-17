8a2f1a39dc0cb6eaef4429d2b6c72d2f4437724b
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-05-17 23:49:27 +0530

    update the cart.jsx component

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -64,7 +64,7 @@ export const CartContextProvider = ({ children }) => {
   function removeItemFromCart(id) {
     dispatchActions({ type: 'REMOVE_ITEM', id });
   }
-
+ 
   function clearCart() {
     dispatchActions({ type: 'CLEAR_CART' });
   }
