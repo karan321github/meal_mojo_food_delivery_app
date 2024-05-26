f37c69738906911f996c8fe3f6da695b25ca346a
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-05-26 23:50:04 +0530

    add router functionality using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-// import { Router } from "express";
 // import Login from "./components/auth/Login";
 // import { Route } from "react-router-dom";
 import Products from "./components/Products";
 import Header from "./components/Header";
 import Cart from "./components/Cart";
 import Checkout from "./components/Checkout";
+import { Route, Routes, Router } from "react-router-dom";
+import SignUp from "./components/auth/SignUp";
+import LogIn from "./components/auth/LogIn";
 
 function App() {
   return (
     <>
       <Header />
-      <Products />
-      <Cart />
-      <Checkout />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        {/* You can add a NoMatch component for unmatched routes */}
+        {/* <Route path="*" element={<NoMatch />} /> */}
+      </Routes>
     </>
   );
 }
