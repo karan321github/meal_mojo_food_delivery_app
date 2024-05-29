5b7965bab39a49c08bd79b8fb71ce1ca8260d281
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-05-30 00:05:40 +0530

    install some packages and corrections

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CartContextProvider } from "./store/CartContext.jsx";
 import { BrowserRouter } from "react-router-dom";
-import {store} from './redux/store.js' 
+import { store } from "./redux/store.js";
 import App from "./App.jsx";
 import "./index.css";
 import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <UserProgressContextProvider>
       <CartContextProvider>
-        <App store={store}/>
+        <Provider store={store}>
+          <App />
+        </Provider>
       </CartContextProvider>
     </UserProgressContextProvider>
   </BrowserRouter>
