f747f1380cee5f44a550596ed2a85bbf13ae5e3b
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-05-31 18:39:03 +0530

    update home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import chocolateBrownie from "../assets/chocolate-brownie.jpg";
 import margritaPizza from "../assets/margherita-pizza.jpg";
-import { Link } from "react-router-dom";
+
+import UserProgressContext from "../store/UserProgressContext";
 
 const Home = () => {
+  const userCtx = useContext(UserProgressContext);
+  const handleLogin = () => {
+    userCtx.showLogin();
+  };
+
+  const handleSignUp = () => {
+    userCtx.showSignUp();
+  }
   return (
     <div
       className="relative h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url(${margritaPizza})` }}
+      style={{ backgroundImage: `url(${chocolateBrownie})` }}
     >
       {/* Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
       {/* Navbar */}
       <nav className="relative z-10 flex justify-between items-center p-5 bg-transparent">
-        <h1 className="text-2xl text-white font-bold">
-          mojo meals
-        </h1>
-        <div>
-          <Link to="/login" className="text-white mr-4 hover:underline">
+        <h1 className="text-2xl text-white font-bold">mojo meals</h1>
+        <div className=" mx-20">
+          <button
+            onClick={handleLogin}
+            className="text-white mr-4 hover:underline"
+          >
             Login
-          </Link>
-          <Link to="/signup" className="text-white hover:underline">
+          </button>
+          <button onClick={handleSignUp} className="text-white hover:underline">
             Sign Up
-          </Link>
+          </button>
         </div>
       </nav>
 
       {/* Main Content */}
       <div className="relative z-10 flex justify-center items-center h-full">
-        <h1 className="text-7xl text-white font-bold">
+        <h1 className="text-3xl font-extrabold italic -mt-20 text-white">
           mojo meals
         </h1>
       </div>
