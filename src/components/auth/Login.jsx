3c063840476f53c1f88a559c91d1171372686d7c
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-05-31 18:39:38 +0530

    make a Modal for login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import Modal from "../UI/Modal";
+import Input from "../UI/Input";
+import UserProgressContext from "../../store/UserProgressContext";
+import Button from "../UI/Button";
 
 const LogIn = () => {
-  return <div>LogIn</div>;
+  const userCtx = useContext(UserProgressContext);
+  return (
+    <Modal className=" max-w-md" open={userCtx.progress === "login"}>
+    <form className=""action="">
+      <Input label="Email" type="email" />
+      <Input label="Password" type="password" />
+      <Button className=" mt-2">Login</Button>
+      {/* <Button className='text-only'>close</Button> */}
+    </form>
+  </Modal>
+  );
 };
 
 export default LogIn;
