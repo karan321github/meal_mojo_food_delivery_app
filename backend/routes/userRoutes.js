7eb4f95be52ce714fb3549eaf1c038251625667b
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-05-31 23:51:19 +0530

    add routes for login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import registerUser from "../controllers/userController.js";
+import { registerUser, login } from "../controllers/userController.js";
 
 const router = express.Router();
 router.post("/signup", registerUser);
-
+router.post("/login", login);
 
 export default router;
